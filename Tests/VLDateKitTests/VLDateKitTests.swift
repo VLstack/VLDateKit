415a1fe15b092290e9698ddf216e89240c61a576
VLstack <github@vlstack.fr>
2024-03-20 14:27:50 +0100

    add test for "yesterday()"

--- Tests/VLDateKitTests/VLDateKitTests.swift
+++ Tests/VLDateKitTests/VLDateKitTests.swift
@@ -1,12 +1,36 @@
 import XCTest
 @testable import VLDateKit
 
-final class VLDateKitTests: XCTestCase {
-    func testExample() throws {
-        // XCTest Documentation
-        // https://developer.apple.com/documentation/xctest
-
-        // Defining Test Cases and Test Methods
-        // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
-    }
+final class VLDateKitTests: XCTestCase 
+{
+//    func testExample() throws {
+//        // XCTest Documentation
+//        // https://developer.apple.com/documentation/xctest
+//
+//        // Defining Test Cases and Test Methods
+//        // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
+//    }
+ 
+ func testYesterday()
+ {
+  var dateComponents: DateComponents = .init()
+  dateComponents.year = 2024
+  dateComponents.month = 5
+  dateComponents.day = 15
+  dateComponents.hour = 17
+  dateComponents.minute = 54
+  dateComponents.second = 32
+  dateComponents.nanosecond = 923
+  
+  guard let date: Date = Calendar.current.date(from: dateComponents)
+  else { XCTFail("Cannot create base date"); return }
+  
+  guard let yesterday: Date = date.yesterday()
+  else { XCTFail("Cannot create yesterday"); return }
+  
+  guard let dateMinus1: Date = Calendar.current.date(byAdding: .day, value: -1, to: date)
+  else { XCTFail("Cannot create dateMinus1"); return }
+  
+  XCTAssert(yesterday.isSame(dateMinus1, toGranularity: .day), "Yesterday is invalid")
+ }
 }
