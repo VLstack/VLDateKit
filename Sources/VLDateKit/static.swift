37971db77b71443d5197424aa1b1c3238da248fc
VLstack <github@vlstack.fr>
2024-03-25 08:34:59 +0100

    add static methods

--- Sources/VLDateKit/static.swift
+++ Sources/VLDateKit/static.swift
@@ -1,8 +1,46 @@
-//
-//  File.swift
-//  
-//
-//  Created by Laurent Vilday on 25/03/2024.
-//
-
 import Foundation
+
+public extension Date
+{
+ static var firstLetterOfWeekday: [ String ]
+ {
+  let calendar = Calendar.current
+  let weekdays = calendar.shortWeekdaySymbols
+  
+  return weekdays.map
+  {
+   weekday in
+   guard let firstLetter = weekday.first
+   else { return "" }
+   
+   return String(firstLetter)
+  }
+ }
+ 
+ static var firstLetterOfWeekDayCapitalized: [ String ]
+ {
+  firstLetterOfWeekday.map { $0.capitalized }
+ }
+ 
+ static var firstLetterOfWeekDayLowered: [ String ]
+ {
+  firstLetterOfWeekday.map { $0.localizedLowercase }
+ }
+
+ static var fullMonthNames: [ String ]
+ {
+  let formatter = DateFormatter()
+  formatter.locale = Locale.current
+  formatter.setLocalizedDateFormatFromTemplate("MMMM")
+  
+  return (1...12).compactMap
+  {
+   month in
+   guard let date = Calendar.current.date(from: DateComponents(year: 2000, month: month, day: 1))
+   else { return "?" }
+   
+   return formatter.string(from: date)
+//   return date.map { formatter.string(from: $0) }
+  }
+ }
+}
