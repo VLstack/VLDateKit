4d7d3535dedeea78cf8f838c6398f827b18cc7d8
VLstack <github@vlstack.fr>
2024-04-10 19:07:34 +0200

    improve readability

--- Package.swift
+++ Package.swift
@@ -3,24 +3,16 @@
 
 import PackageDescription
 
-let package = Package(
-    name: "VLDateKit",
-    platforms: [
-            .iOS(.v17)
-        ],
-    products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
-        .library(
-            name: "VLDateKit",
-            targets: ["VLDateKit"]),
-    ],
-    targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
-        .target(
-            name: "VLDateKit"),
-        .testTarget(
-            name: "VLDateKitTests",
-            dependencies: ["VLDateKit"]),
-    ]
-)
+let package = Package(name: "VLDateKit",
+                      platforms: [ .iOS(.v17) ],
+                      products:
+                      [
+                       .library(name: "VLDateKit",
+                                targets: ["VLDateKit"])
+                      ],
+                      targets:
+                      [
+                       .target(name: "VLDateKit"),
+                       .testTarget(name: "VLDateKitTests",
+                                   dependencies: ["VLDateKit"])
+                      ])
